d287c6aff3b90146aaa24bcebaf09e74022e2d85
AlvesRichard <richard.alves1994@gmail.com>
2021-07-14 16:41:43 -0300

    all routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 
 router.use('/auth', require('./auth'))
 router.use('/recuiters', require('./recuiters'))
-router.use('/companies',require('./companies'))
+router.use('/companies', require('./companies'))
+router.use('/jobs',require('./jobs'))
 
 
 module.exports = router;
