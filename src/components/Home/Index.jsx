2154d1fd40bd020db0519c84bcb5115348fffb22
Lucas Cisternas <l.cisternasopazo@gmail.com>
2021-07-15 14:37:08 -0400

    navbar and login view

--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import style from './index.module.css'
 import { Link } from 'react-router-dom'
 import NavBar from '../Navbar/Index'
+import Footer from '../Footer/Index'
 
 const Home = () => {
   return (
-    <div className='container'>
-      <div className={style.navbar}>
-        <h1>Navbar</h1> 
-      </div>
+    <div className={style.container} >
+      
+        <NavBar />
+      
       <div className={style.home}>
         <h1>Home</h1>
+      </div>
+      <div className={style.footer}>
+        <Footer />
       </div>
     </div>
   )
