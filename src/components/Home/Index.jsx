5bad91bd495361799a65dcb7e4865b97e5ea278d
Lucas Cisternas <l.cisternasopazo@gmail.com>
2021-07-15 11:18:50 -0400

    litle change

--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import style from './index.module.css'
 import { Link } from 'react-router-dom'
+import NavBar from '../Navbar/Index'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Soy el Home</h1>
-      <Link to='/login'>
-        <button>Login</button>
-      </Link>
+    <div className='container'>
+      <div className={style.navbar}>
+        <h1>Navbar</h1> 
+      </div>
+      <div className={style.home}>
+        <h1>Home</h1>
+      </div>
     </div>
   )
 }
