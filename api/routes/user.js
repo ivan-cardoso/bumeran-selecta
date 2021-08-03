6936ded86fd4587bba7429828de559357e069b95
AlvesRichard <richard.alves1994@gmail.com>
2021-08-03 17:58:30 -0300

    editar usuario

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,66 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../db/models/users')
-const Roles = require('../db/models/roles')
-const { firebase, admin } = require('../firebase')
+const userController = require("../controllers/userController");
 
 // Create a storage reference from our storage service
 
-router.post('/register', (req, res) => {
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(req.body.email, req.body.password)
-    .then((userCredential) => {
-      const user = userCredential.user
+router.post("/register", userController.register);
 
-      return Roles.findOne({ where: { name: req.body.role } }).then((role) => {
-        return User.create({
-          ...user,
-          name: req.body.name,
-          surname: req.body.surname,
-          img: req.body.img,
-        }).then((user) => {
-          role.addUser(user)
-          res.json(user)
-        })
-      })
-    })
-    .catch((err) => res.json(err.code))
-})
+router.delete("/delete/:uid", userController.deleteByUid);
 
-router.delete('/delete/:uid', (req, res) => {
-  admin
-    .auth()
-    .deleteUser(req.params.uid)
-    .then((userRecord) => {
-      User.destroy({ where: { uid: req.params.uid } }).then(() =>
-        res.sendStatus(200)
-      )
-    })
-    .catch((error) => {
-      console.log('Error fetching user data:', error)
-    })
-})
+router.get("/all", userController.getAll);
 
-router.get('/all', (req, res) => {
-  User.findAll({ include: Roles })
-    .then((user) => res.json(user))
-    .catch((err) => console.log(err))
-})
+router.get("/:uid", userController.getOneByUid);
 
-router.get('/:uid', (req, res) => {
-  User.findOne({ where: { uid: req.params.uid }, include: Roles })
-    .then((user) => res.json(user))
-    .catch((err) => console.log(err))
-})
-router.put('/update/:uid', (req, res, next) => {
-  const { name, surname, img, roleId } = req.body
-  User.update(
-    { name, surname, img, roleId },
-    { where: { uid: req.params.uid }, returning: true }
-  )
-    .then((user) => res.status(200).json(user[1]))
-    .catch((err) => next(err))
-})
+router.put("/update/:uid", userController.updateByUid);
 
 module.exports = router
--- src/components/Navbar/Index.jsx
+++ src/components/Navbar/Index.jsx
@@ -1,74 +1,116 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { UserLogout } from '../../store/user/user'
-import s from './index.module.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { UserLogout } from "../../store/user/user";
+import s from "./index.module.css";
 /* Imports Material-UI */
-import Modal from '@material-ui/core/Modal'
-import { makeStyles } from '@material-ui/core/styles'
-import Avatar from '@material-ui/core/Avatar'
-import Backdrop from '@material-ui/core/Backdrop'
-import Fade from '@material-ui/core/Fade'
-import BtnLogin from '../UX/Buttons/BtnLogin'
-import BtnLogout from '../UX/Buttons/BtnLogout'
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import BtnLogin from "../UX/Buttons/BtnLogin";
+import BtnLogout from "../UX/Buttons/BtnLogout";
+import ImageUpload from "../RecruiterForm/ImageUpload";
+import { message } from "antd";
+import axios from "axios";
+
 
 /* Functions Material-UI */
 function getModalStyle() {
-  const top = 50
-  const left = 50
+  const top = 50;
+  const left = 50;
 
   return {
     top: `${top}%`,
     left: `${left}%`,
     transform: `translate(-${top}%, -${left}%)`,
-  }
+  };
 }
 
 const useStyles = makeStyles((theme) => ({
   modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   paper: {
-    position: 'absolute',
+    position: "absolute",
     width: 600,
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
-}))
+}));
 
 /* End Functions */
 
 const NavBar = () => {
-  const user = useSelector((state) => state.user)
-  const dispatch = useDispatch()
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
   /* Material-UI state and functions */
-  const [modalStyle] = React.useState(getModalStyle)
-  const [open, setOpen] = React.useState(false)
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+  const [edit, setEdit] = React.useState(false);
+  const [values, setValues] = React.useState({})
+  
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
 
-  const classes = useStyles()
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+  const classes = useStyles();
 
   const handleOpen = () => {
-    setOpen(true)
-  }
+    setOpen(true);
+  };
   const handleClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
   /* End state and functions */
 
   const handleLogout = () => {
-    dispatch(UserLogout(user))
-  }
+    dispatch(UserLogout(user));
+  };
 
+  React.useEffect(() => {
+    setValues(user)
+  },[user])
+
+  const handleEdit = () => {
+    if (edit) {
+      setEdit(false);
+    }
+    else {
+      setEdit(true);
+    }
+  };
+  const sumbitEdit = () => {
+    if (
+      values.name !== "" &&
+      values.surname !== "" &&
+      values.img !== "" &&
+      values.email !== ""
+    ) {
+      axios.put(`/api/user/update/${values.uid}`, values).then(res => res.data).then(userUpdated => {
+        //setValues({...userUpdated[0]})
+        setEdit(false)
+      message.success("Usuario editado con exito");
+      })
+    } else {
+      message.warning("Complete los campos");
+    }
+  }
   return (
     <div className={s.navbarContainer}>
-      <Link to={'/'}>
+      <Link to={"/"}>
         <img
-          src='https://www.bumeran.com.ar/selecta/wp-content/uploads/2021/06/logo-2.png'
-          alt='bumeran-selecta-logo'
+          src="https://www.bumeran.com.ar/selecta/wp-content/uploads/2021/06/logo-2.png"
+          alt="bumeran-selecta-logo"
         />
       </Link>
 
@@ -76,17 +118,17 @@ const NavBar = () => {
         <div className={s.navbarButtonsContainer}>
           <button
             className={s.buttonPerfil}
-            type='button'
+            type="button"
             onClick={() => handleOpen()}
           >
             <Avatar />
           </button>
-          <BtnLogout name='Logout' onClick={handleLogout}></BtnLogout>
+          <BtnLogout name="Logout" onClick={handleLogout}></BtnLogout>
         </div>
       ) : (
         <div>
-          <Link to={'/login'}>
-            <BtnLogin name='Login'></BtnLogin>
+          <Link to={"/login"}>
+            <BtnLogin name="Login"></BtnLogin>
           </Link>
         </div>
       )}
@@ -94,7 +136,7 @@ const NavBar = () => {
       <Modal
         open={open}
         onClose={() => {
-          handleClose()
+          handleClose();
         }}
         className={classes.modal}
         closeAfterTransition
@@ -107,38 +149,90 @@ const NavBar = () => {
           <div style={modalStyle} className={classes.paper}>
             <div className={s.profileUser}>
               <h1>Sobre mi...</h1>
-              {user && (
-                <img
-                  style={{ maxWidth: '200px' }}
-                  src={user.img}
-                  alt={user.name}
-                />
+              {edit ? (
+                <ImageUpload values={values} setValues={setValues} />
+              ) : (
+                values && (
+                  <img
+                    style={{ maxWidth: "200px" }}
+                    src={values.img}
+                    alt={values.name}
+                  />
+                )
               )}
               <h3>
                 <h3 className={s.stylePersonalInfo}>Nombre y Apellido</h3>
-                <span>{user && user.name} </span>
-                <span> {user && user.surname}</span>
+                {edit ? (
+                  <input
+                    className={s.inputs}
+                    onChange={handleInputChange}
+                    name="name"
+                    type="text"
+                    value={values.name}
+                  />
+                ) : (
+                  <span>{values && values.name} </span>
+                )}
+                {edit ? (
+                  <input
+                    className={s.inputs}
+                    onChange={handleInputChange}
+                    name="surname"
+                    type="text"
+                    value={values.surname}
+                  />
+                ) : (
+                  <span> {values && values.surname}</span>
+                )}
               </h3>
 
               <h3>
                 <h3 className={s.stylePersonalInfo}> Email </h3>
-                <span>{user && user.email}</span>{' '}
+                {edit ? (
+                  <input
+                    className={s.inputs}
+                    onChange={handleInputChange}
+                    name="email"
+                    type="email"
+                    value={values.email}
+                  />
+                ) : (
+                  <span>{values && values.email}</span>
+                )}
               </h3>
               <h3>
                 <h3 className={s.stylePersonalInfo}>Rol</h3>
-                <span>{user && user.role.name} </span>
+                <span>{values.id && values.role.name} </span>
               </h3>
-
-              <Link to='/forgotpassword'>
-                <h3>
+              <div className={s.buttons}>
+                {edit ? (
+                  <button className={s.buttonEdit} onClick={sumbitEdit}>
+                    Confirmar
+                  </button>
+                ) : (
+                  <Link to="/forgotpassword">
+                    <button
+                      className={s.buttonChangePassword}
+                      onClick={() => handleClose()}
+                    >
+                      Cambiar Contrasena{" "}
+                    </button>
+                  </Link>
+                )}
+                {edit ? (
                   <button
                     className={s.buttonChangePassword}
-                    onClick={() => handleClose()}
+                    onClick={handleEdit}
+                    style={{ width: "30%" }}
                   >
-                    Cambiar Contrasena{' '}
+                    Cancelar
                   </button>
-                </h3>
-              </Link>
+                ) : (
+                  <button className={s.buttonEdit} onClick={handleEdit}>
+                    Editar
+                  </button>
+                )}
+              </div>
             </div>
           </div>
         </Fade>
@@ -152,7 +246,7 @@ const NavBar = () => {
         </div>
       )} */}
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
